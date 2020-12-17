35606507e7b34f8bff87a1b0a8c1dced7fca8a5c
Taehee Choi <cth5545cth5545@gmail.com>
2020-12-16 23:24:07 -0800

    implementation completed (ver. 1)

--- models/author.js
+++ models/author.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
 
+const { DateTime } = require("luxon");
+
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema({
@@ -14,16 +16,30 @@ AuthorSchema.virtual("name").get(function () {
   return this.family_name + ", " + this.first_name;
 });
 
-// Virtual for author's lifespan
-AuthorSchema.virtual("lifespan").get(function () {
-  return (
-    this.date_of_death.getYear() - this.date_of_birth.getYear()
-  ).toString();
-});
-
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
   return "/catalog/author/" + this._id;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "?";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "?";
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual("lifespan").get(function () {
+  return (this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "?") + "-" + (this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "?");
 });
 
 //Export model
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
 
+const { DateTime } = require("luxon");
+
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema({
@@ -11,12 +13,16 @@ var BookInstanceSchema = new Schema({
     enum: ["Available", "Maintenance", "Loaned", "Reserved"],
     default: "Maintenance",
   },
-  due_back: { type: Date, default: Date.now },
+  due_back: { type: Date, default: Date.now() },
 });
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
   return "/catalog/bookinstance/" + this._id;
+});
+
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
